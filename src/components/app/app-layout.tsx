04b4edaff77dac8ff3ec965acfaec88f0d1046f7
Katarzyna Święty <katarzyna.swiety@gmail.com>
2026-01-10 05:03:15 +0000

    no change

--- src/components/app/app-layout.tsx
+++ src/components/app/app-layout.tsx
@@ -108,7 +108,7 @@ export default function AppLayout({ children }: AppLayoutProps) {
         setTrailRadioOpen={setTrailRadioOpen} 
         hasUnreadMessages={hasUnreadMessages}
       />
-      <main className="flex-1 overflow-hidden">{childrenWithProps}</main>
+      <main className="flex-1">{childrenWithProps}</main>
       <UserProfileSheet open={profileOpen} onOpenChange={setProfileOpen} />
       <InboxSheet 
         open={inboxOpen} 
