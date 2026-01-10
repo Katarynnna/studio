92736720bb98e271eef7de5ff6270015de1e7f85
Katarzyna Święty <katarzyna.swiety@gmail.com>
2026-01-10 15:16:18 +0000

    you added bottom padding, revert that

--- src/components/app/inbox-sheet.tsx
+++ src/components/app/inbox-sheet.tsx
@@ -118,12 +118,6 @@ export default function InboxSheet({ open, onOpenChange, messages, addMessageToI
              </SheetTitle>
         </SheetHeader>
         
-        {!selectedConversationId && messages.length === 0 && 
-             <div className="px-6">
-                <SheetDescription>You have no new messages.</SheetDescription>
-            </div>
-        }
-        
         <div className="flex-1 flex flex-col overflow-hidden">
             <div className="flex-1 overflow-y-auto px-6">
                     {!selectedConversationId ? (
